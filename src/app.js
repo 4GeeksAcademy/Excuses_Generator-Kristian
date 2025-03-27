eb821506d7d451f31e6d7700e7903946ecce711e
Kristian Pacheco <kristianpachecar18@gmail.com>
2025-03-27 03:07:12 +0000

    Last Update to show on the website my bad excuse

--- src/app.js
+++ src/app.js
@@ -5,7 +5,22 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+
+
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+let quien = Math.floor(Math.random() * who.length);
+let accion = Math.floor(Math.random() * action.length);
+let que = Math.floor(Math.random() * what.length);
+let cuando = Math.floor(Math.random() * when.length);
+
+let statement = who[quien] + " " + action[accion] + " " + what[que] + " " + when[cuando];
+let new_statement = document.getElementById("excuse");
+new_statement.innerHTML = statement
 };
