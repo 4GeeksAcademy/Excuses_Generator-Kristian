fc3065f36ca01327a7a2444dbc77ba7e04695aaa
Kristian Pacheco <kristianpachecar18@gmail.com>
2025-04-08 03:02:38 +0000

    Added new function for replacing variables where math floor and math random where stored for each part of the final statement

--- src/app.js
+++ src/app.js
@@ -15,10 +15,7 @@ const action = ['ate', 'peed', 'crushed', 'broke'];
 const what = ['my homework', 'my phone', 'the car'];
 const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-let quien = Math.floor(Math.random() * who.length);
-let accion = Math.floor(Math.random() * action.length);
-let que = Math.floor(Math.random() * what.length);
-let cuando = Math.floor(Math.random() * when.length);
+const getRandomExcuse = arr => arr[Math.floor(Math.random() * arr.length)];
 
 let statement = who[quien] + " " + action[accion] + " " + what[que] + " " + when[cuando];
 let new_statement = document.getElementById("excuse");
