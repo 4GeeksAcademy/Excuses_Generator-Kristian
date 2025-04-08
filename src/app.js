2080992289b7991430719afe5fd709d7a696bdcc
Kristian Pacheco <kristianpachecar18@gmail.com>
2025-04-08 03:25:40 +0000

    Formatted document for a better understanding

--- src/app.js
+++ src/app.js
@@ -10,15 +10,15 @@ window.onload = function () {
 
 
 
-const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-const action = ['ate', 'peed', 'crushed', 'broke'];
-const what = ['my homework', 'my phone', 'the car'];
-const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  const action = ['ate', 'peed', 'crushed', 'broke'];
+  const what = ['my homework', 'my phone', 'the car'];
+  const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-const getRandomExcuse = arr => arr[Math.floor(Math.random() * arr.length)];
+  const getRandomExcuse = arr => arr[Math.floor(Math.random() * arr.length)];
 
-let statement = `${getRandomExcuse(who)} ${getRandomExcuse(action)} ${getRandomExcuse(what)} ${getRandomExcuse(when)}.`;
+  let statement = `${getRandomExcuse(who)} ${getRandomExcuse(action)} ${getRandomExcuse(what)} ${getRandomExcuse(when)}.`;
 
-let new_statement = document.getElementById("excuse");
-new_statement.innerHTML = statement
+  let new_statement = document.getElementById("excuse");
+  new_statement.innerHTML = statement
 };
